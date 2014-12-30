05ebb3ea47847ae721b4e3cc0df679b6b45e4f1a
Luciano Bargmann <luciano.bargmann@gmail.com>
2014-12-30 01:42:22 -0200

    CORS

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
+            new Nelmio\CorsBundle\NelmioCorsBundle(),
             new FOS\RestBundle\FOSRestBundle(),
             new HcktPlanet\FoundationBundle\HcktPlanetFoundationBundle(),
         );
